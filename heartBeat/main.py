87dd1acae68d9438e7e9d00991bacaa8f6dafbde
Jesse <jelloeater@gmail.com>
2014-06-18 18:29:40 -0400

    Started fiddling with direct calls to MineOS (thanks hexparrot)

--- heartBeat/main.py
+++ heartBeat/main.py
@@ -1,15 +1,30 @@
+#!/usr/bin/env python2.7
+"""A python project to manage Minecraft servers hosted on MineOS (http://minecraft.codeemo.com)
+"""
+__author__ = "Jesse S"
+__license__ = "GNU GPL v2.0"
+__version__ = "0.1b"
+__email__ = "jelloeater@gmail.com"
+
+
 import logging
 from socket import timeout
 import os
 from time import sleep
+import sys
 from minecraft_query import MinecraftQuery
+from mineos import mc
 
-__author__ = 'Jesse S'
+logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)", level=logging.DEBUG)
 
-logging.basicConfig(filename="heartBeat.log", format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)", level=logging.INFO)
+logging.debug(sys.path)
 
 
 def main():
+
+	serverList = mc.list_servers("/var/games/minecraft")
+	print(serverList)
+
 	address = "localhost"
 	port = 25565
 	logging.info("Starting monitor")
